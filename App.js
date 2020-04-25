c8387ff5da0409d5923f70b593bd067bb92dcc9e
shrey <shreystechtips@gmail.com>
2020-04-24 23:57:09 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.box}>
+        <View
+          style={{
+            backgroundColor: "#FCFFFC",
+            height: 10,
+            marginLeft: "25%",
+            marginRight: "25%",
+            borderRadius: 10,
+            marginBottom: 15,
+          }}
+        />
+        <Text style={{ color: "#ffffff" }}>
+          Open up App.js to start working on your app!
+        </Text>
+        <Button
+          title="Press me"
+          onPress={() =>
+            Alert.alert(
+              "Simple Button pressed",
+              "Description",
+              [
+                {
+                  text: "Ask me later",
+                  onPress: () => console.log("Ask me later pressed"),
+                },
+                {
+                  text: "Cancel",
+                  onPress: () => console.log("Cancel Pressed"),
+                  style: "cancel",
+                },
+                { text: "OK", onPress: () => console.log("OK Pressed") },
+              ],
+              { cancelable: false }
+            )
+          }
+        />
+        <View style={{ height: 200, width: "100%" }} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FCFFFC",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    color: "#fff",
+  },
+  box: {
+    borderTopLeftRadius: 50,
+    borderTopEndRadius: 50,
+    backgroundColor: "#2D3A3A",
+    padding: 30,
+    bottom: 0,
+    width: "97%",
   },
 });
